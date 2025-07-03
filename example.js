374ac880fd64cad80ed71d4db39c6f8df5f3370b
simonM21 <simonmattekkatt21@gmail.com>
2025-07-03 18:21:14 +0530

    made changes to the example page and few major changes

--- example.js
+++ example.js
@@ -1,80 +1,65 @@
 import { Animator, htmlToObj } from "./Tideon.js";
 
-const animator = new Animator();
-const box1 = new htmlToObj("box1");
-const box2 = new htmlToObj("box2");
+let animators = [];
 
-animator.addStage({
-  func: function () {
-    return animator.animationSequence([
-      animator.animateFunc(1, () => {
-        box1.setAll({
-          x: 10,
-          y: 50,
-          opacity: 0,
-          width: 100,
-          height: 100,
-          rotation: 0,
-        });
+for (let i = 0; i < 8; i++) {
+  animators.push(new Animator());
+}
 
-        box2.setAll({
-          x: 200,
-          y: 50,
-          opacity: 1,
-        });
-      }),
-      animator.delay(50),
-      animator.to(60, [
-        {
-          obj: box1,
-          changes: {
-            x: 200,
-            opacity: 1,
-            rotation: 90,
-            width: 200,
-            height: 200,
-          },
-        },
-      ]),
-      animator.delay(20),
-      //   animator.animate(60, [{ obj: box1, changes: { x: 300 } }]),
-      animator.animate(80, [
-        {
-          obj: box1,
-          changes: { x: 500 },
-          parameters: { ease: "easeOut" },
-        },
-      ]),
-      animator.to(60, [
-        {
-          obj: box1,
-          changes: {
-            opacity: 0,
-          },
-        },
-      ]),
-    ]);
-  },
-});
-animator.standAloneFunc(1, () => {
-  box2.setAll({
-    x: 200,
-    y: 50,
-    opacity: 1,
+let boxes = [];
+for (let i = 0; i < 8; i++) {
+  boxes.push(new htmlToObj(`box${i}`));
+}
+
+const easeFunctionNames = [
+  "linear",
+  "easeIn",
+  "easeOut",
+  "easeInOut",
+  "easeInCubic",
+  "easeOutCubic",
+  "easeInOutCubic",
+  "easeInExpo",
+  "easeOutExpo",
+  "easeInOutExpo",
+  "easeInBack",
+  "easeOutBack",
+  "easeInOutBack",
+  "easeOutElastic",
+  "drag",
+  "bounce",
+  "easeOutBounce",
+  "easeInBounce",
+  "easeInOutBounce",
+];
+
+for (let i = 0; i < animators.length; i++) {
+  animators[i].standAloneFunc(1, () => {
+    boxes[i].setAll({
+      x: 200,
+      y: 90,
+      opacity: 1,
+    });
   });
-});
-animator.standAloneAnimate(150, [
-  { obj: box2, changes: { x: 800 }, parameters: { ease: "drag" } },
-]);
-animator.standAloneDelay(20);
-animator.standAloneAnimate(20, [{ obj: box2, changes: { opacity: -1 } }]);
-animator.standAloneFunc(1, () => {
-  box2.setAll({
-    x: 200,
-    y: 50,
-    opacity: 1,
-  });
+  animators[i].standAloneDelay(20);
+  animators[i].standAloneAnimate(150, [
+    {
+      obj: boxes[i],
+      changes: { x: 1000 },
+      parameters: { ease: easeFunctionNames[i] },
+    },
+  ]);
+  animators[i].standAloneDelay(20);
+  animators[i].standAloneAnimate(50, [
+    { obj: boxes[i], changes: { opacity: -1 } },
+  ]);
+}
+
+//--------------------------------------------------------------------------------
+
+animators.forEach((ani) => {
+  ani.shouldLoop = true;
+  ani.mainLoop(10);
 });
 
-animator.shouldLoop = true;
-animator.mainLoop(10);
+//--------------------------------------------------------------------------------
